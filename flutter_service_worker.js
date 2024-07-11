a19add96beac38091397c4aadba5a68a491472bb
EftiarHKhan <mdeftiar.official@gmail.com>
2024-07-11 10:14:30 +0600

    base url changed

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -27,8 +27,8 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "afc9d19a8c67c782093f9eea8f200fd9",
-"/": "afc9d19a8c67c782093f9eea8f200fd9",
+"index.html": "a5423cd18236bc37f7f0a7f6a8d432d6",
+"/": "a5423cd18236bc37f7f0a7f6a8d432d6",
 "main.dart.js": "4d6fe43716afea06b12c9bb528183ef8",
 "manifest.json": "2fb9188af0bb263135877d3e3a776209",
 "version.json": "9094aacdae789dccd67fa32109ff1a18"};
